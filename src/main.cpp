cf8f043566d23727b863df6f64e2f0d8768a7018
sfjuocekr <sfjuocekr@gmail.com>
2022-02-03 02:05:51 +0100

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -37,25 +37,24 @@ String byteToString(byte *_byte, unsigned _len)
 void callback(char *topic, byte *payload, unsigned int length) // payload is 1 of 0
 {
   String _payload = byteToString(payload, length);
-  Serial.println("MOTOR from topic: " + String(topic[partTopic.length() ]));
   unsigned _motorNumber = String(topic[partTopic.length()]).toInt();
 
-  Serial.println("Message arrived [" + String(topic) + "]: " + _payload);
-  Serial.println("MOTOR actuated: " + String(_motorNumber ));
+  Serial.println(F("[callback] Message arrived [") + String(topic) + F("]: ") + _payload);
+  // Serial.println(F("MOTOR: ") + String(_motorNumber));
 
   switch (_payload[0])
   {
   case '0':                         // off
     digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level
-    Serial.println(F("\tChannel OFF: ") + String(_motorNumber ));
-    MQTTclient.publish(String(partTopic + String(_motorNumber) + F("/state")).c_str(), "OFF");
+    // Serial.println(F("\tChannel OFF: ") + String(_motorNumber));
+    MQTTclient.publish(String(partTopic + String(_motorNumber) + F("/state")).c_str(), "OFF"); // Je had deze ook gewoon 0 kunnen maken, ipv dat in die andere config
     channel[_motorNumber] = false;
     motors[_motorNumber]->stop();
     break;
   case '1':                          // on
     digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off (Note that LOW is the voltage level
-    Serial.println(F("\tChannel ON: ") + String(_motorNumber ));
-    MQTTclient.publish(String(partTopic + String(_motorNumber) + F("/state")).c_str(), "ON");
+    // Serial.println(F("\tChannel ON: ") + String(_motorNumber));
+    MQTTclient.publish(String(partTopic + String(_motorNumber) + F("/state")).c_str(), "ON"); // net als deze
     channel[_motorNumber] = true;
     motors[_motorNumber]->forward();
     break;
@@ -66,47 +65,44 @@ void threadPublishCallback()
 {
   float _CurrentPh = 0;
 
-  // Serial.print(F("threadPublishCallback: ") + String(millis() / 1000));
+  // Serial.print(F("[threadPublishCallback] ") + String(millis() / 1000));
   //
-  // Serial.print(F("\tCurrentPh: ") + String(_CurrentPh));
-  MQTTclient.publish(String(F("sensors/") + clientId + F("/CurrentPh")).c_str(), String(_CurrentPh).c_str());
+  // Serial.print(F("\tCurrentPh: ") + String(_CurrentPh)); // dit is altijd nul
+  // MQTTclient.publish(String(F("sensors/") + clientId + F("/CurrentPh")).c_str(), String(_CurrentPh).c_str()); // dit is altijd 0
 
   // JSON Payload
 
-  String _payload = "{\"clientId\":";
+  String _payload = F("{\"clientId\":");
   _payload += (String)clientId;
 
-  _payload += ",\"CurrentPh\":";
+  _payload += F(",\"CurrentPh\":");
   _payload += String(_CurrentPh);
 
-  _payload += ",\"Ch1\":";
+  _payload += F(",\"Ch1\":");
   _payload += String(channel[0]);
 
-  _payload += ",\"Ch2\":";
+  _payload += F(",\"Ch2\":");
   _payload += String(channel[1]);
 
-  _payload += ",\"Ch3\":";
+  _payload += F(",\"Ch3\":");
   _payload += String(channel[2]);
 
-  _payload += ",\"Ch4\":";
+  _payload += F(",\"Ch4\":");
   _payload += String(channel[3]);
+
+  _payload += F("}");
 
   // Serial.println(F("\ttime: ") + String(timeClient.getEpochTime()));
   MQTTclient.publish(String(F("sensors/") + clientId + F("/debug/connected")).c_str(), String(timeClient.getEpochTime()).c_str());
-
-  _payload += "}";
-
   MQTTclient.publish(String(F("sensors/") + clientId + F("/json")).c_str(), _payload.c_str());
 }
 
-void subscribeMQTT()  // Doe hier je subscriptions
+void subscribeMQTT() // Doe hier je subscriptions
 {
   for (unsigned _i = 0; _i < 4; _i++)
   {
-    MQTTclient.subscribe(String(partTopic + String(_i ) + F("/command")).c_str());
-    // debugging subscrition topics...
-    Serial.print(F("Subscribing to: "));
-    Serial.println(String(partTopic + String(_i) + F("/command")).c_str());
+    MQTTclient.subscribe(String(partTopic + String(_i) + F("/command")).c_str());
+    // Serial.println(F("[subscribeMQTT] Subscribing to: ") + String(partTopic + String(_i) + F("/command")).c_str());
   }
 }
 
@@ -115,13 +111,9 @@ boolean reconnect() // Called when client is disconnected from the MQTT server
   if (MQTTclient.connect(clientId.c_str()))
   {
     timeClient.update(); // Is this the right time to update the time, when we lost connection?
-
-    // Serial.print(F("reconnect: ") + String(millis() / 1000));
-    // Serial.println(F("\ttime: ") + String(timeClient.getEpochTime()));
+    // Serial.print(F("[reconnect] ") + String(millis() / 1000) + F("\ttime: ") + String(timeClient.getEpochTime()));
     MQTTclient.publish(String(F("sensors/") + clientId + F("/debug/connected")).c_str(), String(timeClient.getEpochTime()).c_str());
-
     subscribeMQTT();
-
     threadPublishCallback(); // Publish after reconnecting, probably not necesary with small LOGPERIOD
   }
 
@@ -131,7 +123,7 @@ boolean reconnect() // Called when client is disconnected from the MQTT server
 String generateClientIdFromMac() // Convert the WiFi MAC address to String
 {
   byte _mac[6];
-  String _output = "";
+  String _output = F("");
 
   WiFi.macAddress(_mac);
 
@@ -148,7 +140,7 @@ void setup()
   Serial.begin(115200);
   // Serial.print("SDA: ");Serial.print(SDA);
   // Serial.print(" SCL: ");Serial.println(SCL);
-  Serial.println("Connecting to: " + (String)ssid);
+  Serial.println(F("[setup] Connecting to: ") + (String)ssid);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -158,13 +150,13 @@ void setup()
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print(".");
+    Serial.print(F("."));
     delay(500);
   }
   Serial.println();
 
-  Serial.println("WiFi connected!");
-  Serial.println("IP address: ");
+  Serial.println(F("WiFi connected!"));
+  Serial.println(F("IP address: "));
   Serial.println(WiFi.localIP());
 
   MQTTclient.setServer(mqtt_server, mqtt_port);
@@ -184,9 +176,9 @@ void setup()
   {
     for (unsigned _i = 0; _i < 4; _i++)
     {
-      Serial.println("Setup channel: " + String(_i));
+      // Serial.println(F("Setup channel: ") + String(_i));
 
-      motors[_i] = new extDcMotor(_i * 2, _i * 2 + 1 ); // dit zijn je motoren!!!
+      motors[_i] = new extDcMotor(_i * 2, _i * 2 + 1); // dit zijn je motoren!!!
       motors[_i]->begin(&mcp1);
     }
   }
